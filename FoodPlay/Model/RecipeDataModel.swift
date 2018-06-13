87f114be0465d56600f2f85e8605697a08ebc782
chekmate <waltmitchvt@gmail.com>
2018-06-12 21:40:57 -0400

    First commit of Food Play

---
+++ FoodPlay/Model/RecipeDataModel.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeDataModel.swift
+//  FoodPlay
+//
+//  Created by Walter Mitchell on 6/6/18.
+//  Copyright © 2018 Walter Mitchell. All rights reserved.
+//
+
+import Foundation
+
+class RecipeDataModel{
+    
+    var imageUrl: String = ""
+    var id: Int = 0
+    var body: String = ""
+    var title: String = ""
+    var servings: Int = 0
+    var cookTime: Int = 0
+    
+    
+    
+  
+    
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FoodPlay' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FoodPlay
+
+  #JSON handling for REST/Api responses
+  pod 'SwiftyJSON'
+  #HTTP networking library
+  pod 'Alamofire', '~> 4.5'
+
+end
